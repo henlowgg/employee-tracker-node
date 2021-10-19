cadc4d0f29e463d402ea216edd071373b253bfc0
Jack Ault <henlowgg@outlook.com>
2021-10-19 09:00:46 -0400

    wrote function to start application

--- index.js
+++ index.js
@@ -15,9 +15,58 @@ const menuOptions = [
             "View all employees",
             "Add a department",
             "Add a role",
-            "Add an employeee",
+            "Add an employee",
             "Update an employee role",
             "Quit",
         ],
     },
 ];
+
+// function starting the application itself
+
+function start() {
+    console.log("Welcome to the Employee Tracker!");
+    inquirer
+    .prompt(menuOptions)
+    .then((response) => {
+        console.log(response);
+        switch (response.mainMenu) {
+            case "View all departments":
+                queries.viewDepartments();
+                break;
+
+            case "View all roles":
+                queries.viewRoles();
+                break;
+
+            case "View all employees":
+                queries.viewEmployees();
+                break;
+            
+            case "Add a department":
+                queries.addDepartment();
+                break;
+
+            case "Add a role":
+                queries.addRole();
+                break;
+
+            case "Add an employee":
+                queries.addEmployee();
+                break;
+
+            case "Update an employee role":
+                queries.updateRole();
+                break;
+
+            case "Quit":
+                queries.quit();
+                break;
+        
+            default:
+                break;
+        }
+    });
+}
+
+start();
