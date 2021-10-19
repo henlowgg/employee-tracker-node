ea6bc079871e4efdf7df97d2b9731469c41784e4
Jack Ault <henlowgg@outlook.com>
2021-10-19 09:25:18 -0400

    schema built

---
+++ db/schema.sql
@@ -0,0 +1,37 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+DROP TABLE IF EXISTS department;
+CREATE TABLE department (
+    id INT AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+DROP TABLE IF EXISTS role;
+CREATE TABLE role (
+    id INT AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(department_id)
+    REFERENCES department(id)
+);
+
+DROP TABLE IF EXISTS employee;
+CREATE TABLE role (
+    id INT AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT DEFAULT NULL,
+    PRIMARY KEY(id),
+
+    FOREIGN KEY(role_id)
+    REFERENCES role(id)
+
+    ON DELETE SET NULL
+);
