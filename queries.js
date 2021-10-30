4a0ba76156612675e95edb45d8db2b6953f2df56
Jack Ault <henlowgg@outlook.com>
2021-10-30 16:30:57 -0400

    queries creating tables

--- queries.js
+++ queries.js
@@ -1,1 +1,44 @@
 const preB = require('./db/connections');
+
+const queries = { 
+allDepts: async function () {
+    const db = await preB;  
+    const [results, fields] = await db.execute('SELECT * FROM department');
+    console.table(results);
+    return results
+  
+},
+
+retDepts: async function () {
+    const db = await preB;  
+    const [results, fields] = await db.execute('SELECT * FROM department');
+    return results;
+  
+},
+
+allRoles: async function () {
+    const db = await preB;
+    const [results, fields] = await db.execute('SELECT * FROM role');
+    console.table(results);
+    return results;
+},
+
+retRoles: async function () {
+    const db = await preB;
+    const [results, fields] = await db.execute('SELECT * FROM role');
+    return results;
+},
+
+allEmployees: async function () {
+    const db = await preB;
+    const [results, fields] = await db.execute('SELECT * FROM employee');
+    console.table(results);
+    return results;
+},
+
+retEmployees: async function () {
+    const db = await preB;
+    const [results, fields] = await db.execute('SELECT * FROM employee');
+    return results;
+},
+
